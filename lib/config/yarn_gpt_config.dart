c495e16e4e3dcb534730e30d385d66247b24d8aa
Cirrus CI <support@cirruslabs.org>
2026-06-07 00:48:30 +0000

    fixed bug

--- lib/config/yarn_gpt_config.dart
+++ lib/config/yarn_gpt_config.dart
@@ -3,34 +3,14 @@ library yarn_gpt_config;
 
 class YarnGptConfig {
   static String get apiKey {
-    final envApiKey = const String.fromEnvironment(
-      'YARNGPT_API_KEY',
-      defaultValue: '',
-    );
+    const envApiKey = String.fromEnvironment('YARNGPT_API_KEY');
 
     if (envApiKey.isNotEmpty) {
       return envApiKey;
     }
 
-    if (_localYarnGptApiKey.isNotEmpty) {
-      return _localYarnGptApiKey;
-    }
-
-    if (_testApiKey != null && _testApiKey!.isNotEmpty) {
-      return _testApiKey!;
-    }
-
     throw StateError(
-      'YarnGPT API Key not configured. '
-      'Set YARNGPT_API_KEY or lib/config/yarn_gpt_config.local.dart.',
+      'YARNGPT_API_KEY is not set in environment variables',
     );
   }
-
-  static String? _testApiKey;
-
-  static void setTestApiKey(String key) {
-    _testApiKey = key;
-  }
-
-  static String? getTestApiKey() => _testApiKey;
 }
