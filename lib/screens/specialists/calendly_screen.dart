480b2866e95e801b93987f9b648a921bec4348fe
Sandra Aframea Ofosu <168097776+Aframia@users.noreply.github.com>
2026-07-31 11:21:51 +0100

    added theme file

--- lib/screens/specialists/calendly_screen.dart
+++ lib/screens/specialists/calendly_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import "../../theme/app_colors.dart";
 import 'package:webview_flutter/webview_flutter.dart';
 
 class CalendlyScreen extends StatefulWidget {
